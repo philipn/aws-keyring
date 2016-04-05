b9487ae14a9e96e94f5804b4af51d15120f363bd
Philip Neustrom <philipn@gmail.com>
2016-04-05 15:24:41 -0700

    v0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aws-keyring',
-    version='0.2.2',
+    version='0.2.3',
     description='Manage AWS credentials in your OS keyring.',
     author='Philip Neustrom',
     author_email='philipn@gmail.com',
